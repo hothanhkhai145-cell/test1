e58fb2dead05f9c6bcc992b9d80a8b1022d5cf68
nguyenhoangtien <DH52003694@student.stu.edu.vn>
2025-12-18 23:53:41 +0700

    hi

--- index.php
+++ index.php
@@ -13,8 +13,8 @@ include 'login_handler.php';
 <body>
 <div id="templatemo_top_panel">
 	<div id="templatemo_top_section">
-		<div id="site_title">Xin chào</div> 
-        <div id="site_tagline">Nguyễn Hùng Thanh Sơn - Thứ 4 ca 2</div>  
+		<div id="site_title">khai</div> 
+        <div id="site_tagline">khai - Thứ 4 ca 2</div>  
         <div id="templatemo_menu">
         	<ul>
                 <li><a href="index.php" class="current">Home</a></li>
